f9af845555e9c2a701bfa905e80dd3aea42132d1
unknown <ptlergo@gmail.com>
2018-10-21 20:52:14 -0400

    backend init. babel, package dependencies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+import { MatToolbarModule } from '@angular/material';
+
+// config. routes
+const routes: Routes = [
+  {path: 'create', component: CreateComponent},
+  {path: 'list', component: ListComponent},
+  {path: 'edit/:id', component: EditComponent},
+  {path: '', redirectTo: '/list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -16,9 +25,12 @@ import { EditComponent } from './components/edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
