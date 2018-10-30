69894ac23a040e41d38e3648d494c7736c8a0564
unknown <ptlergo@gmail.com>
2018-10-30 13:44:39 -0400

    added material design imports, added methods for accessing issue service observable

--- frontend/src/app/components/list/list.component.ts
+++ frontend/src/app/components/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
+
+import { Issue } from '../../issue.model';
 import { IssueService } from '../../issue.service';
 @Component({
   selector: 'app-list',
@@ -7,13 +11,31 @@ import { IssueService } from '../../issue.service';
 })
 export class ListComponent implements OnInit {
 
+  issues: Issue[];
+  displayedColumns = ['title', 'response', 'severity', 'status', 'actions'];
+
   // inject the service to use it
-  constructor(private issueService: IssueService) { }
+  constructor(private issueService: IssueService, private router: Router) { }
 
   ngOnInit() {
-    this.issueService.getIssues().subscribe((issues) => {
-      console.log('ssd');
-      console.log(issues);
+    this.fetchIssues();
+  }
+
+  fetchIssues(): void {
+    this.issueService.getIssues().subscribe((data: Issue[]) => {
+      this.issues = data;
+      console.log('Data requested...');
+      console.log(this.issues);
+    });
+  }
+
+  editIssue(id: number): void {
+    this.router.navigate([`/edit/${id}`]);
+  }
+
+  deleteIssue(id: number): void {
+    this.issueService.deleteIssue(id).subscribe(() => {
+      this.fetchIssues();
     });
   }
 
